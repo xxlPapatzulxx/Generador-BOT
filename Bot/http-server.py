3fe91999f474fe561e1af8cef4b4dab7db912bc0
JerrySBG <165622937+JerrySBG@users.noreply.github.com>
2025-03-01 14:20:05 -0500

    1.0

--- Bot/http-server.py
+++ Bot/http-server.py
@@ -1,7 +1,7 @@
 #!/bin/bash
 IVAR="/etc/http-instas"
 onliCHECK=/var/www/html/ChumoGH
-LIST="$(echo "NewVPS-" | rev)"
+LIST="$(echo "J3RRY-" | rev)"
 [[ -d /var/www/html/ChumoGH ]] || mkdir ${onliCHECK}
 install_fun () {
 apt-get install netcat -y
